66ab60639b2b390223cafa6b408e27333c95cd20
user <254574@astanait.edu.kz>
2026-09-13 17:19:51 +0500

    Commit 3: Character attributes, constructor and getters

--- src/Character.java
+++ src/Character.java
@@ -1,3 +1,47 @@
 public class Character {
+    private String name;
+    private String characterClass;
+    private int health;
+    private int mana;
+    private int level;
+    private String weapon;
+    private String armor;
 
+    public Character(String name, String characterClass, int health, int mana, int level, String weapon, String armor){
+        this.name = name;
+        this.characterClass = characterClass;
+        this.health = health;
+        this.mana = mana;
+        this.level = level;
+        this.weapon = weapon;
+        this.armor = armor;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getCharacterClass(){
+        return characterClass;
+    }
+
+    public int getHealth(){
+        return health;
+    }
+
+    public int getMana(){
+        return mana;
+    }
+
+    public int getLevel(){
+        return level;
+    }
+
+    public String getWeapon(){
+        return weapon;
+    }
+
+    public String getArmor(){
+        return armor;
+    }
 }
